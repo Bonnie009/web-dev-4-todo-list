00e7ebaf91faa4e7a82d8f46d8ae6ce86d9a9dd3
Amelie <peach.panja@gmail.com>
2022-01-04 17:17:37 -0700

    Setting up Next.js

---
+++ to-do-list/components/new-list/new-list.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import styled from "styled-components";
+
+const NewList = () => {
+    return (
+        <div>Add stuff here</div>
+    );
+};
+
+export default NewList;
---
+++ to-do-list/pages/index.jsx
@@ -0,0 +1,22 @@
+import Head from 'next/head'
+import styles from '../styles/Home.module.css'
+// import components
+// import List from '../components/list/list'
+// import NewList from '../components/new-list/new-list'
+// import ProgressBar from '../components/progress-bar/progress-bar'
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>To do list</title>
+        <meta name="description" content="Web Dev 4 project" />
+      </Head>
+      <h1>Hello Everyone!</h1>
+      {/* add your components here */}
+      {/* <List />
+      <NewList />
+      <ProgressBar /> */}
+    </div>
+  )
+}
