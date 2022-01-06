96d26ad3cbb9a9424f8487b9b31df803ffed503f
Bonnie <bonnie.dam4@gmail.com>
2022-01-05 21:32:13 -0800

    Created the new-list component

--- to-do-list/components/new-list/new-list.jsx
+++ to-do-list/components/new-list/new-list.jsx
@@ -1,9 +1,32 @@
 import React from "react";
 import styled from "styled-components";
 
-const NewList = () => {
+// + New To-Do button
+const ListItem = styled.button`
+    width:auto;
+    height:auto;
+    background-color:transparent;
+    border:none;
+    cursor: pointer;
+    font-size:24px;
+`
+
+const NewList = ({
+    subtitle="+ New To-Do"
+}) => {
+    const AddNew = () =>{
+        const ul = document.getElementById("list"); //Gets the whole list ul
+        const li = document.createElement("li"); //Create new list item
+        li.appendChild(document.createTextNode("Subtitle")); //Appends the li item to ul
+        ul.appendChild(li);
+    }
     return (
-        <div>Add stuff here</div>
+        <div>Add stuff here
+            <ul id="list">
+                
+            </ul>
+            <ListItem onClick={AddNew}>{subtitle}</ListItem>
+        </div>
     );
 };
 
