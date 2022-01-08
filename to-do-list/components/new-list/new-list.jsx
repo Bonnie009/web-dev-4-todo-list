c61cdaf14c9dce7f6ded92c67054806b91076ca3
Bonnie <bonnie.dam4@gmail.com>
2022-01-07 17:17:42 -0800

    changed new list to useState

--- to-do-list/components/new-list/new-list.jsx
+++ to-do-list/components/new-list/new-list.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 import styled from "styled-components";
 
 // + New To-Do button
@@ -14,20 +14,32 @@ const ListItem = styled.button`
 const NewList = ({
     subtitle="+ New To-Do"
 }) => {
-    const AddNew = () =>{
-        const ul = document.getElementById("list"); //Gets the whole list ul
-        const li = document.createElement("li"); //Create new list item
-        li.appendChild(document.createTextNode("Subtitle")); //Appends the li item to ul
-        ul.appendChild(li);
+    // const AddNew = () =>{
+    //     const ul = document.getElementById("list"); //Gets the whole list ul
+    //     const li = document.createElement("li"); //Create new list item
+    //     li.appendChild(document.createTextNode("Subtitle")); //Appends the li item to ul
+    //     ul.appendChild(li);
+    // }
+
+    const [items, setItems] = useState([])
+
+    const createItems = () => {
+        setItems(oldItems => [...oldItems, {
+            id:1,
+            title:"subtitle"
+        }])
     }
+
     return (
-        <div>Add stuff here
+        <div>
             <ul id="list">
-                
+                {items.map(item => (
+                    <li key={item.id}>{"subtitle"}</li>
+                ))}
             </ul>
-            <ListItem onClick={AddNew}>{subtitle}</ListItem>
+            <ListItem onClick={createItems}>{subtitle}</ListItem>
         </div>
     );
 };
-
+ 
 export default NewList;
